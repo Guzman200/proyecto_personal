278d03f776de583bad3d899efc6043d13800ced1
Pedro Guzman <my.rg.developer@gmail.com>
2020-06-03 21:04:12 -0500

    Primer commit, navegacion responsiva

---
+++ modelos/plantillas/navegacion.php
@@ -0,0 +1,16 @@
+<!-- NAVEGACION -->
+<nav class="navbar navbar-dark bg-dark navbar-expand-sm">
+    <button class="navbar-toggler" type="button" data-toggle="collapse"                    data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <a class="navbar-brand mr-auto ml-4 font-weight-bold" href="#">BBVA Bancomer</a>
+    <div class="collapse navbar-collapse" id="navbarToggleExternalContent">
+        <div class="navbar-nav bg-dark p-1 ml-auto mr-0">
+            <a class="nav-item nav-link mr-2" href="#">Facturas</a>
+            <a class="nav-item nav-link mr-2" href="#">Seguridad</a>
+            <a class="nav-item nav-link mr-2" href="#">Servicios</a>
+            <a class="nav-item nav-link" href="#">Contacto</a>
+        </div>
+    </div>
+</nav>
